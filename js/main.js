5efe9d3da98e18a4352ff2856d545b8157f6ad51
danklingjr <danielklingjr@gmail.com>
2015-02-10 16:42:42 -0500

    Responsive styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
 
+		mywindow.scroll(function(){
+		  newscroll = mywindow.scrollTop();
+
+		    if (newscroll > 300 && !up) {
+		        $( ".phantom" ).fadeIn(300);
+		
+		        up = !up;
+		        
+		      } else if(newscroll < 300 && up) {
+		        $( ".phantom" ).fadeOut(150);
+		        up = !up;
+		      }
+		      mypos = newscroll;
+		      return false;
+
+		  
+		});
